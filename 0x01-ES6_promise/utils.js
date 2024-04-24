9170510a5c45a73ef10c479a300320982ac9ee5d
LeinadMori <iromdaniel@gmail.com>
2024-04-24 11:54:27 +0100

    include uploadPhoto returns a response with the format

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,12 +1,14 @@
-module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
-        },
-      ],
-    ],
-  };  
+export function uploadPhoto() {
+    return Promise.resolve({
+      status: 200,
+      body: 'photo-profile-1',
+    });
+  }
+  
+  
+  export function createUser() {
+    return Promise.resolve({
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    });
+  }
